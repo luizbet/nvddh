02421689754e2ba660eba24d3d4f872754d14980
luizbet <luiz.bet@unesp.br>
2024-08-03 12:42:27 -0300

    1

--- login/config_db.php
+++ login/config_db.php
@@ -1,14 +1,13 @@
 <?php
 
     $dbHost = 'http://nvddh.com.br/u237680592_nvddh_db';
-    $dbUsername = 'u237680592_nvddh_db_main';
+    $dbUsername = 'u237680592_nvddh_db_dev';
     $dbPassword = 'Anvddh11071903#';
     $dbName = 'nvddhusrs';
 
     $connect_db = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName)
 
-    if ($connect_db->connect_errno)
-    {
+    if ($connect_db->connect_errno){
         echo "erro";
     }
     else {
