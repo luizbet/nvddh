e69a358c49e1864d4970b9f84914b28838da12d4
luizbet <luiz.bet@unesp.br>
2024-08-03 14:25:15 -0300

    1

--- dbecon.php
+++ dbecon.php
@@ -1,18 +1,15 @@
 <?php
-$servername = "154.49.247.59";
-$database = "nvddh_db";
-$username = "nvddh_db_dev";
-$password = "Anvddh11071903#";
 
-// Create connection
+    $servername = "154.49.247.59";
+    $database = "u237680592_nvddh_db";
+    $username = "u237680592_nvddh_db_dev";
+    $password = "Anvddh11071903#";
 
 $conn = mysqli_connect($servername, $username, $password, $database);
-
-// Check connection
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 echo "Connected successfully";
 mysqli_close($conn);
-?>  
+?>
