95309ef03537699fc1e1653a00c9bb3da847ce69
luizbet <luiz.bet@unesp.br>
2024-08-03 12:09:20 -0300

    1

---
+++ login/config_db.php
@@ -0,0 +1,10 @@
+<?php
+
+    $dbHost = 'nvddh.com.br';
+    $dbUsername = 'u237680592_nvddh_db_main';
+    $dbPassword = 'Anvddh11071903#';
+    $dbName = 'nvddhusrs';
+
+    $connect_db = new mysqli()
+
+?>
