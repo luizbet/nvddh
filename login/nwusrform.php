1388e26291769728a8e28e2ef12f5e3f432bcc4f
luizbet <luiz.bet@unesp.br>
2024-08-03 16:22:21 -0300

    1

--- login/nwusrform.php
+++ login/nwusrform.php
@@ -3,21 +3,21 @@
     if(isset($_POST['submit']))
     {
         print_r('Nome: ' - $_POST['nome']);
-        print_r('<br>')
+        print_r('<br>');
         print_r('Nome: ' - $_POST['sobrenome']);
-        print_r('<br>')
+        print_r('<br>');
         print_r('Nome: ' - $_POST['usrname']);
-        print_r('<br>')
+        print_r('<br>');
         print_r('Nome: ' - $_POST['email']);
-        print_r('<br>')
+        print_r('<br>');
         print_r('Nome: ' - $_POST['senha']);
-        print_r('<br>')
+        print_r('<br>');
         print_r('Nome: ' - $_POST['biousr']);
-        print_r('<br>')
+        print_r('<br>');
         print_r('Nome: ' - $_POST['lattesusr']);
-        print_r('<br>')
+        print_r('<br>');
         print_r('Nome: ' - $_POST['lattesid']);
-        print_r('<br>')
+        print_r('<br>');
     }
 
 ?>
