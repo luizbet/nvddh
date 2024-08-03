f31222ae7f7a846aca76b37c78b3271547df5502
luizbet <luiz.bet@unesp.br>
2024-08-03 14:20:22 -0300

    1

---
+++ databaseconnect.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "154.49.247.59";
+$database = "u237680592_nvddh_db";
+$username = "u237680592_nvddh_db_dev";
+$password = "Anvddh11071903#";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+mysqli_close($conn);
+?>
